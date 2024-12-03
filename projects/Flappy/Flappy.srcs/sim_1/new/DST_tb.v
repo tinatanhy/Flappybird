04d4204ab7819f0efca3ea29f401b50f3e97db5a
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-12-03 16:37:42 +0800

    Add DST_tb.v

--- projects/Flappy/Flappy.srcs/sim_1/new/DST_tb.v
+++ projects/Flappy/Flappy.srcs/sim_1/new/DST_tb.v
@@ -1,26 +1,41 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2024/12/02 20:28:47
-// Design Name: 
-// Module Name: DST_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module DST_tb();
 
+reg clk, rstn, rstn_clk;
+initial begin
+    clk = 0;
+    rstn = 0;
+    rstn_clk = 0;
+    #10;
+    rstn_clk = 1;
+    forever #5 clk = ~clk;
+end
 
-module DST_tb(
+initial begin
+    #100000;
+    rstn = 1;
+end
 
-    );
+wire pclk, locked;
+
+ClkWizPCLK clkwiz_pclk
+(
+    // Clock out ports
+    .clk_out1(pclk),     // output clk_out1
+    // Status and control signals
+    .resetn(rstn_clk), // input reset
+    .locked(locked),       // output locked
+    // Clock in ports
+    .clk_in1(clk)      // input clk_in1
+);
+
+wire hen, ven, hs, vs;
+DST dst (
+    .rstn(rstn),
+    .pclk(pclk),
+
+    .hen(hen),        //水平显示有效
+    .ven(ven),        //垂直显示有效
+    .hs(hs),         //行同步
+    .vs(vs)          //场同步
+);
 endmodule
