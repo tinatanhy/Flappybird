094827162964caca6221bdcbbca45cc4aa5651af
tinatanhy <2184572598@qq.com>
2024-12-03 19:16:38 +0800

    Add button module

--- projects/Flappy/Flappy.srcs/sources_1/new/Top.v
+++ projects/Flappy/Flappy.srcs/sources_1/new/Top.v
@@ -1,7 +1,8 @@
 module Top(
-    input CLK100MHZ, CPU_RESETN,
+    input CLK100MHZ, CPU_RESETN,BTNC,BTNU,BTNL,
     output VGA_HS, VGA_VS,
-    output [3:0] VGA_R, VGA_G, VGA_B
+    output [3:0] VGA_R, VGA_G, VGA_B,
+    output [2:0] LED
 );
 ViewCore viewcore(
     .clk (CLK100MHZ),
@@ -10,4 +11,14 @@ ViewCore viewcore(
     .vs  (VGA_VS),
     .rgb ({ VGA_R, VGA_G, VGA_B })
 );
+wire [2:0] led_w; // 声明一个 wire 来连接 LED 的输出  
+
+Button button_instance ( 
+    .clk (CLK100MHZ),  
+    .BTNC (BTNC),  
+    .BTNU (BTNU),  
+    .BTNL (BTNL),  
+    .LED (LED)   
+); 
+
 endmodule
