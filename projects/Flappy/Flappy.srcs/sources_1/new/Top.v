9a9d2fe1c1f72b600aa309e0003046a0212c458d
rubatotree <zhuyutian@mail.ustc.edu.cn>
2024-12-03 22:55:13 +0800

    Revert "Merge branch 'thy'"
    This reverts commit ea202b786c37ff5330193f8139d6b13d17826685, reversing
    changes made to 8309f87c9b7b9b44e211c649036397f9bbb09419.

--- projects/Flappy/Flappy.srcs/sources_1/new/Top.v
+++ projects/Flappy/Flappy.srcs/sources_1/new/Top.v
@@ -1,8 +1,7 @@
 module Top(
-    input CLK100MHZ, CPU_RESETN,BTNC,BTNU,BTNL,
+    input CLK100MHZ, CPU_RESETN,
     output VGA_HS, VGA_VS,
-    output [3:0] VGA_R, VGA_G, VGA_B,
-    output [2:0] LED
+    output [3:0] VGA_R, VGA_G, VGA_B
 );
 ViewCore viewcore(
     .clk (CLK100MHZ),
@@ -11,14 +10,4 @@ ViewCore viewcore(
     .vs  (VGA_VS),
     .rgb ({ VGA_R, VGA_G, VGA_B })
 );
-wire [2:0] led_w; // 声明一个 wire 来连接 LED 的输出  
-
-Button button_instance ( 
-    .clk (CLK100MHZ),  
-    .BTNC (BTNC),  
-    .BTNU (BTNU),  
-    .BTNL (BTNL),  
-    .LED (LED)   
-); 
-
 endmodule
